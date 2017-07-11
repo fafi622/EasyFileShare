8a83e981c1874b60f36e7391d0e5034908d60476
Ramphis Abraham <xfafyx622@gmail.com>
2017-07-11 00:34:06 -0500

    Set up server object

---
+++ ProjectFiles/FileShareServer/FileShareServer/ServerObject.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+namespace FileShareServer
+{
+    
+    class ServerObject
+    {
+        private Socket Server;
+        private IPEndPoint Address;
+        private bool isAsync;
+        public ServerObject(string LocalIpv4Address, int Port,bool isA = true)
+        {
+            Server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Address = new IPEndPoint(IPAddress.Parse(LocalIpv4Address), Port);
+            isAsync = isA;
+        }
+
+        public void Start()
+        {
+            if (isAsync)
+            {
+                Server.Bind(Address);
+                Server.Listen(100);
+                Server.BeginAccept(null, Server);
+            }
+            else{}
+            
+            
+        }
+    }
+}
