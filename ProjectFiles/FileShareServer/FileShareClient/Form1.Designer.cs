c442603fb7bccce55d5f142700375e2d84318995
fafi622 <xfafyx622@gmail.com>
2017-07-14 02:01:07 -0500

    Did alot of work on the client

--- ProjectFiles/FileShareServer/FileShareClient/Form1.Designer.cs
+++ ProjectFiles/FileShareServer/FileShareClient/Form1.Designer.cs
@@ -28,16 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.IPtextbox = new System.Windows.Forms.TextBox();
             this.IPlabel = new System.Windows.Forms.Label();
             this.ConnectButton = new System.Windows.Forms.Button();
-            this.AlertLable = new System.Windows.Forms.Label();
-            this.FirstPanle = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.connecttionindicater = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.IPlist = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // IPtextbox
             // 
-            this.IPtextbox.Location = new System.Drawing.Point(84, 57);
+            this.IPtextbox.Location = new System.Drawing.Point(70, 55);
             this.IPtextbox.Name = "IPtextbox";
             this.IPtextbox.Size = new System.Drawing.Size(100, 20);
             this.IPtextbox.TabIndex = 0;
@@ -46,7 +53,7 @@
             // IPlabel
             // 
             this.IPlabel.AutoSize = true;
-            this.IPlabel.Location = new System.Drawing.Point(61, 60);
+            this.IPlabel.Location = new System.Drawing.Point(47, 58);
             this.IPlabel.Name = "IPlabel";
             this.IPlabel.Size = new System.Drawing.Size(17, 13);
             this.IPlabel.TabIndex = 1;
@@ -54,7 +61,7 @@
             // 
             // ConnectButton
             // 
-            this.ConnectButton.Location = new System.Drawing.Point(94, 83);
+            this.ConnectButton.Location = new System.Drawing.Point(83, 101);
             this.ConnectButton.Name = "ConnectButton";
             this.ConnectButton.Size = new System.Drawing.Size(75, 23);
             this.ConnectButton.TabIndex = 2;
@@ -62,36 +69,80 @@
             this.ConnectButton.UseVisualStyleBackColor = true;
             this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
             // 
-            // AlertLable
+            // tabControl1
             // 
-            this.AlertLable.AutoSize = true;
-            this.AlertLable.Location = new System.Drawing.Point(114, 145);
-            this.AlertLable.Name = "AlertLable";
-            this.AlertLable.Size = new System.Drawing.Size(0, 13);
-            this.AlertLable.TabIndex = 3;
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(260, 237);
+            this.tabControl1.TabIndex = 4;
             // 
-            // FirstPanle
+            // tabPage1
             // 
-            this.FirstPanle.Location = new System.Drawing.Point(3, 0);
-            this.FirstPanle.Name = "FirstPanle";
-            this.FirstPanle.Size = new System.Drawing.Size(282, 261);
-            this.FirstPanle.TabIndex = 4;
-            this.FirstPanle.Visible = false;
+            this.tabPage1.Controls.Add(this.connecttionindicater);
+            this.tabPage1.Controls.Add(this.IPtextbox);
+            this.tabPage1.Controls.Add(this.IPlabel);
+            this.tabPage1.Controls.Add(this.ConnectButton);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(252, 211);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Connect";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // connecttionindicater
+            // 
+            this.connecttionindicater.BackColor = System.Drawing.Color.Red;
+            this.connecttionindicater.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.connecttionindicater.FlatAppearance.BorderSize = 0;
+            this.connecttionindicater.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.connecttionindicater.Location = new System.Drawing.Point(164, 105);
+            this.connecttionindicater.Name = "connecttionindicater";
+            this.connecttionindicater.Size = new System.Drawing.Size(10, 14);
+            this.connecttionindicater.TabIndex = 3;
+            this.connecttionindicater.Text = "button1";
+            this.connecttionindicater.UseVisualStyleBackColor = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.IPlist);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(252, 211);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Lobby";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // IPlist
+            // 
+            this.IPlist.Location = new System.Drawing.Point(3, 6);
+            this.IPlist.Multiline = true;
+            this.IPlist.Name = "IPlist";
+            this.IPlist.ReadOnly = true;
+            this.IPlist.Size = new System.Drawing.Size(158, 199);
+            this.IPlist.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.FirstPanle);
-            this.Controls.Add(this.AlertLable);
-            this.Controls.Add(this.ConnectButton);
-            this.Controls.Add(this.IPlabel);
-            this.Controls.Add(this.IPtextbox);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FileBuddy";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -100,8 +151,11 @@
         private System.Windows.Forms.TextBox IPtextbox;
         private System.Windows.Forms.Label IPlabel;
         private System.Windows.Forms.Button ConnectButton;
-        private System.Windows.Forms.Label AlertLable;
-        private System.Windows.Forms.Panel FirstPanle;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button connecttionindicater;
+        private System.Windows.Forms.TextBox IPlist;
     }
 }
 
--- ProjectFiles/FileShareServer/FileShareClient/Form1.cs
+++ ProjectFiles/FileShareServer/FileShareClient/Form1.cs
@@ -18,17 +18,44 @@ namespace FileShareClient
         public Form1()
         {
             InitializeComponent();
+            
         }
 
         private void ConnectButton_Click(object sender, EventArgs e)
         {
             address = new IPEndPoint(IPAddress.Parse(IPtextbox.Text), 25567);
             server.Connect(address);
-            AlertLable.Text = "Connected To a server";
-            IPlabel.Visible = false;
-            IPtextbox.Visible = false;
-            ConnectButton.Visible = false;
-            FirstPanle.Visible = true;
+          
+            //string promptValue = Prompt.ShowDialog("Enter IP", "Add Person");
+            
+            tabControl1.SelectTab(1);
+            connecttionindicater.BackColor = Color.Green;
+        }
+
+      
+    }
+
+    public static class Prompt
+    {
+        public static string ShowDialog(string text, string caption)
+        {
+            Form prompt = new Form();
+            
+            prompt.Width = 300;
+            prompt.Height = 200;
+            prompt.StartPosition = FormStartPosition.CenterParent;
+            prompt.Text = caption;
+            Label textLabel = new Label() { Left = 50, Top = 20, Text = text, Width = 200, TextAlign = ContentAlignment.MiddleCenter };
+            TextBox inputBox = new TextBox() { Left = 50, Top = 50, Width = 200 };
+            Button confirmation = new Button() { Text = "Add", Left = 100, Width = 100, Top = 90 };
+            
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(confirmation);
+            prompt.Controls.Add(textLabel);
+            prompt.Controls.Add(inputBox);
+            prompt.ShowDialog();
+            inputBox.Focus();
+            return (string)inputBox.Text;
         }
     }
 }
